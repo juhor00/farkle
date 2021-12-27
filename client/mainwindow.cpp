648b4f6a502e4e858a3e5e262f0f8c433f09c378
juhor <juho.rantala@live.com>
2021-12-28 01:44:02 +0200

    Added ui methods to send and receive events with event handler

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,12 +1,60 @@
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), eventHandler(new EventHandler(this))
+MainWindow::MainWindow(QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    eventHandler(new EventHandler(this))
 {
+    ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+    delete ui;
     delete eventHandler;
+}
+
+void MainWindow::holdDice(unordered_set<string> &dice)
+{
+    eventHandler->createHoldEvent(dice);
+}
+
+void MainWindow::saveDice(unordered_set<string> &dice)
+{
+    eventHandler->createSaveEvent(dice);
+}
+
+void MainWindow::rollDice(std::unordered_set<string> &dice)
+{
+    unordered_set<int> diceInt(dice.size());
+    for(auto& die : dice){
+        diceInt.insert(stoi(die));
+    }
+    rollDice(diceInt);
+}
+
+void MainWindow::showDice(std::unordered_map<string, string> &diceValues)
+{
+    unordered_map<int, int> diceValuesInt(diceValues.size());
+    for(auto& pair : diceValues){
+        diceValuesInt.insert({stoi(pair.first), stoi(pair.second)});
+    }
+    showDice(diceValuesInt);
+}
+
+void MainWindow::bust()
+{
+
+}
+
+void MainWindow::rollDice(unordered_set<int> &dice)
+{
+
+}
+
+void MainWindow::showDice(unordered_map<int, int> &diceValues)
+{
+
 }
 
--- client/mainwindow.h
+++ client/mainwindow.h
@@ -2,7 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
 #include "eventhandler.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+using namespace std;
 
 class EventHandler;
 
@@ -13,8 +22,22 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void holdDice(unordered_set<string>& dice);
+    void saveDice(unordered_set<string>& dice);
+
+public slots:
+    void rollDice(std::unordered_set<std::string>& dice);
+    void showDice(std::unordered_map<std::string, std::string>& diceValues);
+    void bust();
+
+private slots:
+    // Delete this if you prefer to use string instead of int
+    void rollDice(unordered_set<int>& dice);
+    void showDice(unordered_map<int, int>& diceValues);
+
 
 private:
+    Ui::MainWindow* ui;
     EventHandler* eventHandler;
 };
 #endif // MAINWINDOW_H
