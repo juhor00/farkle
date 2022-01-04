247b6bb5482d1a87e5194bc37e8024437808cfdc
juhor <juho.rantala@live.com>
2022-01-04 02:08:06 +0200

    Fixed issues with mingw compiler

--- client/server.h
+++ client/server.h
@@ -3,8 +3,8 @@
 
 #define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
 #include <winsock2.h>
+#include <windows.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
--- utilities.h
+++ utilities.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 namespace utils{
 
