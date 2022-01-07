2d826e767554113bd0ae997fff4835e54a4692e8
juhor <juho.rantala@live.com>
2022-01-07 23:27:45 +0200

    Added event methods

--- server/game/game.cpp
+++ server/game/game.cpp
@@ -3,7 +3,64 @@
 
 
 Game::Game(EventHandler *eventHandler):
-    eventHandler(eventHandler)
+    eventHandler(eventHandler), inTurn(0)
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+bool Game::hold(player player, dice /*dice*/)
+{
+    if(not isInTurn(player)){
+        return false;
+    }
+
+    // CODE HERE
+
+    return true;
+}
+
+bool Game::save(player player, dice /*dice*/)
+{
+    if(not isInTurn(player)){
+        return false;
+    }
+
+    // CODE HERE
+
+    return true;
+}
+
+void Game::changeTurn()
+{
+    inTurn = (inTurn + 1) % 2;
+}
+
+bool Game::isInTurn(player player)
+{
+    return player == inTurn;
+}
+
+void Game::showEvent(diceValues /*dice*/)
+{
+
+}
+
+void Game::bustEvent(player /*player*/)
+{
+
+}
+
+void Game::turnEvent(player /*player*/)
+{
+
+}
+
+void Game::overEvent()
 {
 
 }
--- server/game/game.h
+++ server/game/game.h
@@ -1,16 +1,40 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <unordered_set>
+#include <unordered_map>
+
+using dice = std::unordered_set<int>;
+using diceValues = std::unordered_map<int, int>;
+using player = int;
+
 class EventHandler;
 
 class Game
 {
 public:
     Game(EventHandler* eventHandler);
+    ~Game();
+
+    // Events
+    bool hold(player player, dice dice);
+    bool save(player player, dice dice);
+
 
 private:
 
+    void changeTurn();
+    bool isInTurn(player player);
+
+    // Events
+    void showEvent(diceValues dice);
+    void bustEvent(player player);
+    void turnEvent(player player);
+    void overEvent();
+
+
     EventHandler* eventHandler;
+    player inTurn;
 };
 
 #endif // GAME_H
