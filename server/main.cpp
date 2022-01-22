3da551c672ed736abd3096b841494f277638380b
juhor <juho.rantala@live.com>
2022-01-22 20:27:30 +0200

    Create EventHandler instead of Server

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "server.h"
+#include "eventhandler.h"
 
 using namespace std;
 
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    Server server;
+    EventHandler e;
 
 
     return EXIT_SUCCESS;
